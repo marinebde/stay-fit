f9397cd2bf65265fa38f8baae8e12ab79ad1f198
marinebde <boidemarine@hotmail.fr>
2022-10-19 13:35:10 +0200

    parametrage webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -25,24 +25,6 @@ Encore
     .addEntry('showPartenairejs', './assets/showPartenaire.js')
     .addEntry('indexStructurejs', './assets/indexStructure.js')
     .addEntry('showStructurejs', './assets/showStructure.js')
-    .addStyleEntry('newPartenaire', './assets/styles/newPartenaire.css')
-    .addStyleEntry('editPartenaire', './assets/styles/editPartenaire.css')
-    .addStyleEntry('editModule', './assets/styles/editModule.css')
-    .addStyleEntry('editUser', './assets/styles/editUser.css')
-    .addStyleEntry('indexModule', './assets/styles/indexModule.css')
-    .addStyleEntry('indexPartenaire', './assets/styles/indexPartenaire.css')
-    .addStyleEntry('indexStructure', './assets/styles/indexStructure.css')
-    .addStyleEntry('indexUser', './assets/styles/indexUser.css')
-    .addStyleEntry('login', './assets/styles/login.css')
-    .addStyleEntry('newModule', './assets/styles/newModule.css')
-    .addStyleEntry('newMotDePasse', './assets/styles/newMotDePasse.css')
-    .addStyleEntry('newStructure', './assets/styles/newStructure.css')
-    .addStyleEntry('newUser', './assets/styles/newUser.css')
-    .addStyleEntry('showModule', './assets/styles/showModule.css')
-    .addStyleEntry('showPartenaire', './assets/styles/showPartenaire.css')
-    .addStyleEntry('showStructure', './assets/styles/showStructure.css')
-    .addStyleEntry('showUser', './assets/styles/showUser.css')
-    .addStyleEntry('styleBase', './assets/styles/app.css')
 
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
@@ -80,7 +62,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
